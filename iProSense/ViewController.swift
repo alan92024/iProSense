03688d852ac891ba69ebfac488b48fe0fab89ab5
Alan Singsaas <sensory1@alans-mac-mini-2.local>
2018-10-22 16:18:15 -0700

    Initial Commit

---
+++ iProSense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iProSense
+//
+//  Created by Alan Singsaas on 10/22/18.
+//  Copyright © 2018 Sensory Metrics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
