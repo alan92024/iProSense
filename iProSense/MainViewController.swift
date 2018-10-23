b38b0056eb4d12e47a50edbda95edd89949140fe
Alan Singsaas <sensory1@alans-mac-mini-2.local>
2018-10-22 19:31:11 -0700

    small changes to constraints

--- iProSense/MainViewController.swift
+++ iProSense/MainViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var ageYearsField: UITextField!
+    @IBOutlet weak var ageMonthsField: UITextField!
+    @IBOutlet weak var genderField: UITextField!
+    @IBOutlet weak var handField: UITextField!
+    @IBOutlet weak var childIDField: UITextField!
+    @IBOutlet weak var diagnosisField: UITextField!
+    
+    let ageYearsData = [String](arrayLiteral: "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15")
+    let ageMonthsData = [String](arrayLiteral: "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11")
+    let genderData = [String](arrayLiteral: "Male", "Female")
+    let handData = [String](arrayLiteral: "Left", "Right")
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         <#code#>
@@ -23,6 +34,24 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func createClientDataPickers() {
+        
+        let agePicker = UIPickerView()
+        let genderPicker = UIPickerView()
+        let handPicker = UIPickerView()
+        
+        agePicker.delegate = self
+        genderPicker.delegate = self
+        handPicker.delegate = self
+        
+        
+        
+        ageYearsField.inputView =  agePicker
+        ageMonthsField.inputView = agePicker
+        genderField.inputView = genderPicker
+        handField.inputView = handPicker
     }
 
 
